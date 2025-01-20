ecfd613c2732f76f81a3598b5e31c9acd108f64d
bderbidge <brander81@gmail.com>
2025-01-19 17:48:54 -0700

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Brandon Derbidge on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
